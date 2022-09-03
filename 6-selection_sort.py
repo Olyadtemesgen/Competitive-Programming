1497c5dd47474c75134cbe2361ffd24eecc7f755
Olyadtemesgen <105017653+Olyadtemesgen@users.noreply.github.com>
2022-09-03 10:41:37 +0300

    commit

---
+++ 6-selection_sort.py
@@ -0,0 +1,34 @@
+#User function Template for python3
+
+class Solution: 
+    def __init__(self):
+        pass
+    def select(self, arr, i):
+        smallest = arr[i]
+        for it in range(len(arr))  :
+            if arr[it] < smallest:
+                smallest = arr[it]
+        return smallest  
+    def selectionSort(self, arr,n):
+        new_a = []
+        count = 0
+        for x in range(n):
+            smallest = self.select(arr, i = count)
+            new_a.append(smallest)
+            del arr[arr.index(smallest)]
+        for arras in new_a:
+            arr.append(arras)
+        return arr
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        Solution().selectionSort(arr, n)
+        for i in range(n):
+            print(arr[i],end=" ")
+        print()
+# } Driver Code Ends
