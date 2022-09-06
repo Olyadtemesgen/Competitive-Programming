61ef79bfd4d64468fb51a10c87e264b9a0190e2c
Olyadtemesgen <105017653+Olyadtemesgen@users.noreply.github.com>
2022-09-06 15:25:16 +0300

    commit

--- 21-give_sum_k.py
+++ 21-give_sum_k.py
@@ -2,12 +2,12 @@ class Solution:
     def maxOperations(self, nums, k):
         res = 0
         dict = {}
+        for abc in nums:
+            dict[abc] = 0
         for x in nums:
             if k - x  in dict and dict[k - x] > 0:
                 res += 1
                 dict[k - x] -= 1
             else:
-                if x not in dict:
-                    dict[x] = 0
                 dict[x] += 1
         return res
