4c0f5368688af1d095b462418ebae2ed1639fb70
Olyadtemesgen <105017653+Olyadtemesgen@users.noreply.github.com>
2022-09-07 20:55:02 +0300

    add

---
+++ 26-top_k_frequent.py
@@ -0,0 +1,21 @@
+from collections import Counter
+
+
+class Solution:
+    def topKFrequent(self, nums, k):
+        freq = [[] for i in range(len(nums) + 1)]
+        counter = Counter(nums)
+        new,the = [], []
+        for x in counter:
+            new.append(counter[x])
+            the.append(x)
+        neww = new.copy()
+        sorte = []
+        for x in range(k):
+            sorte.append(new.pop(new.index(max(new))))
+        other = []
+        for abc in sorte:
+            other.append(the[neww.index(abc)])
+            del the[neww.index(abc)]
+            del neww[neww.index(abc)]
+        return other
