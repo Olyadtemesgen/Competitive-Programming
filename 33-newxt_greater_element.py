cb95f5dd6411de54b82f541d5aaeffae664f9fb3
Olyadtemesgen <105017653+Olyadtemesgen@users.noreply.github.com>
2022-09-09 00:27:48 +0300

    commit

--- 33-newxt_greater_element.py
+++ 33-newxt_greater_element.py
@@ -10,3 +10,17 @@ class Solution:
             else:
                 new.append(-1)
         return new
+#this is the best algorithm and it is beautiful man
+    def nextGreaterElement2(self, nums1, nums2):
+        indexer = {j:i for i, j in enumerate(nums1)}
+        result = [0] * len(nums1)
+        stack = []
+        for x in range(len(nums2)):
+            current = nums2[x]
+            while stack and current > stack[-1]:
+                value = stack.pop()
+                nums = indexer[value]
+                result[nums] = current
+            if current in nums1:
+                stack.append(current)
+        return result
