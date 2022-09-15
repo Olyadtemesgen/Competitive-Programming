79fabc7efafd6d0e31dce0364e29801d731565f3
Olyadtemesgen <105017653+Olyadtemesgen@users.noreply.github.com>
2022-09-15 19:20:47 +0300

    commit

--- 37-task_scheduler.py
+++ 37-task_scheduler.py
@@ -6,3 +6,27 @@ class Solution:
         max_ = max(frequency)
         marx_freq = frequency.count(max_)
         return max(len(tasks), (max_ - 1) * (n + 1) + marx_freq)
+#We also can to solve the problem with heapqueue mainly max_heap
+from collections import Counter
+import heapq
+class Solution:
+    def leastInterval(self, tasks, n):
+        result, heap = 0, []
+        counter = Counter(tasks)
+        for key, value in counter.items():
+            heapq.heappush(heap, (-value, key))
+        while heap:
+            i, temp = 0, []
+            while i <= n:
+                result += 1
+                if heap:
+                    v, k = heapq.heappop(heap)
+                    v += 1
+                    if v < 0:
+                        temp.append((v, k))
+                if not heap and not temp:
+                    break
+                i += 1
+            for k, v in temp:
+                heapq.heappush(heap, (k, v))
+        return result
