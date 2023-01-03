4888bada12a87ba2c1913cc40e8e5b6d58968a5b
Olyad Temesgen Amsalu <105017653+Olyadtemesgen@users.noreply.github.com>
2023-01-03 19:42:58 +0300

    Committed By Olyad Temesgen

--- 0043-multiply-strings/0043-multiply-strings.py
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -1,13 +1,27 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        number1 = 0
-        for numberstring in num1:
-            number1 = number1 * 10 + ord(numberstring) - 48
-
-        number2 = 0
-
-        for numberstring in num2:
-            number2 = number2 * 10 + ord(numberstring) - 48
-         
-        return str(number1 * number2)
         
+        int_form_of_num1 = 0
+        
+        #To change num1, which is a string, to the integer having the same characters i.e : changing '123' to 123
+        for int_character in num1:
+            
+            """By using ASCII number of string format of the digits and using 
+            decimal place of the integer.because 10 times the number 
+            increases the decimal number by one """
+            int_form_of_num1 = int_form_of_num1 * 10 + (ord(int_character) - 48)
+
+        int_form_of_num2 = 0
+        
+        #To change num2, which is a string, to the integer having the same characters i.e : changing '123' to 123
+        for int_character in num2:
+            
+            """By using ASCII number of string format of
+            the digits and using decimal point of the integer because
+            10 times the number increases the decimal number by one """
+            int_form_of_num2 = int_form_of_num2 * 10 + ord(int_character) - 48
+        
+        #They are now integers
+        product = int_form_of_num1 * int_form_of_num2)
+        
+        return product
