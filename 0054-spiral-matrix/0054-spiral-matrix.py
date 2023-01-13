d8f0d292eed20c1a4a9f7ad40428fe6910135c04
Olyad Temesgen Amsalu <105017653+Olyadtemesgen@users.noreply.github.com>
2023-01-13 18:41:10 +0300

    Time: 31 ms (85.49%), Space: 14 MB (31.45%) - LeetHub

---
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -0,0 +1,68 @@
+class Solution:
+    
+    def spiralOrder(self, matrix: list[list[int]]) -> list[int]:
+        
+        result = []
+        
+        row = 0
+        column = 0
+        
+        row_finder_r = len(matrix)
+        column_finder_r = len(matrix[0])
+        length = len(matrix) * len(matrix[0])
+        column_finder_l = 0
+        row_finder_l = 0
+        counter = 0
+        while row_finder_r > row_finder_l and column_finder_r >= column_finder_l:
+            print(column_finder_r)
+            first = False
+            second = False
+            third = False
+            fourth = False
+            
+            while len(result) < length and column < column_finder_r:
+            
+                result.append(matrix[row][column])
+                column += 1
+                first = True
+            if first:
+                column -= 1
+                row += 1
+            
+            while len(result) < length and row < row_finder_r:
+                result.append(matrix[row][column])
+                row += 1
+                second = True
+            
+            if second:
+                column -= 1
+                row -= 1
+            
+            while len(result) < length and column >= column_finder_l:
+                result.append(matrix[row][column])
+                column -= 1
+                third = True
+            
+            
+            if third:
+                column += 1
+                row -= 1
+            
+            while len(result) < length and row > row_finder_l:
+                result.append(matrix[row][column])
+                row -= 1
+                fourth = True
+    
+            
+            row += 1
+            column += 1
+
+            row_finder_l += 1
+            column_finder_l += 1
+
+            row_finder_r -= 1
+            column_finder_r -= 1
+            counter += 1
+            if not (first and second and third and fourth):
+                break
+        return result
