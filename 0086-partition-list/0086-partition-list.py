b2e145590175c9c6b9d2158a65c283058652129d
Olyad Temesgen Amsalu <105017653+Olyadtemesgen@users.noreply.github.com>
2023-02-13 17:58:23 +0300

    Time: 39 ms (54.65%), Space: 14 MB (24.19%) - LeetHub

--- 0086-partition-list/0086-partition-list.py
+++ 0086-partition-list/0086-partition-list.py
@@ -6,24 +6,26 @@ class ListNode:
         self.next = next
     
 class Solution:
-    
     def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+        
         array1 = []
         array2 = []
         varr = head
+        
         while head:
             if head.val < x:
                 array1.append(head.val)
                 
             else:
                 array2.append(head.val)
-       
             head = head.next
+        
         if not array1:
             return varr
         
         head = ListNode(array1[0])
         current = head
+        
         for xy in array1[1:]:
             current.next = ListNode(xy)
             current = current.next
@@ -33,3 +35,24 @@ class Solution:
             current = current.next
         
         return head
+    
+    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+        
+        left_ = left = ListNode(0)
+        right_ = right = ListNode(0)
+        
+        while head:
+            if head.val < x:
+                left.next = ListNode(head.val)
+                left = left.next
+            
+            else:
+                right.next = ListNode(head.val)
+                right = right.next
+            
+            head = head.next
+        
+        right.next = None
+        left.next = right_.next
+        
+        return left_.next
