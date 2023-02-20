74bb863d337185af79e84d01f00158275cfa313d
Olyad Temesgen Amsalu <105017653+Olyadtemesgen@users.noreply.github.com>
2023-02-20 22:37:11 +0300

    Time: 48 ms (52.12%), Space: 16.6 MB (68.00%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.py
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.py
@@ -3,31 +3,37 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
     def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         
+        # Create two dummy nodes, odd and even, and store them in startodd and starteven, respectively
         odd = ListNode(0)
         even = ListNode(0)
-        
         startodd = odd
         starteven = even
-        count = 1
         
+        # Initialize count to 1 and iterate over the input linked list
+        count = 1
         while head:
             
+            # If count is odd, append the current node to the odd list
             if count % 2:
                 odd.next = head
                 head = head.next
                 odd = odd.next
                 odd.next = None
             
+            # If count is even, append the current node to the even list
             else:
                 even.next = head
                 head = head.next
                 even = even.next
                 even.next = None
                 
+            # Increment count by 1
             count += 1
-        odd.next = starteven.next
         
+        # Concatenate the odd and even lists and return the modified linked list
+        odd.next = starteven.next
         return startodd.next
