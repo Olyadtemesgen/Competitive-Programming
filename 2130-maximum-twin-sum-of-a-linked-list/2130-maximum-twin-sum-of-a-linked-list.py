20e07721719fd373905da52f6f36b08a6d778100
Olyad Temesgen Amsalu <105017653+Olyadtemesgen@users.noreply.github.com>
2023-02-20 23:56:00 +0300

    Time: 892 ms (85.67%), Space: 45.1 MB (72.43%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.py
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.py
@@ -4,8 +4,36 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+    
     def pairSum(self, head: Optional[ListNode]) -> int:
         
+        #first let us find the mid and reverse the half mid
+        slow = head
+        fast = head
+        
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+        
+        current = slow
+        previous = None
+        
+        #this code reverse the above half of the linked list
+        while current:
+            current.next, previous, current = previous, current, current.next
+        
+        maximum = 0
+        
+        while previous:
+            
+            maximum = max(maximum, previous.val + head.val)
+            head = head.next
+            previous = previous.next
+        
+        return maximum
+    #this works but with space complexity of O(N)
+    def pairSum1(self, head: Optional[ListNode]) -> int:
+
         array = []
         
         while head:
