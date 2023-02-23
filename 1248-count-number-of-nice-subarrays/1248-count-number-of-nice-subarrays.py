ee189d9b294b4ba8d6c78c6c2cd87d15c1970f6c
Olyad Temesgen Amsalu <105017653+Olyadtemesgen@users.noreply.github.com>
2023-02-23 16:27:31 +0300

    Time: 837 ms (79.94%), Space: 20.7 MB (72.44%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
@@ -9,6 +9,7 @@ class Solution:
         
         lll = left
         for right in range(len(nums)):
+            
             summ += (nums[right] % 2)
             
             if summ == k:
@@ -24,25 +25,3 @@ class Solution:
                 
         return result
         ###############################
-        left = count = nice_count = odd_count = 0
-        
-        for j in range(len(nums)):
-            
-            if nums[j] % 2:
-                
-                odd_count += 1
-                
-                if odd_count == k:
-                    count = 0
-                    
-                    while odd_count == k:
-                        
-                        if nums[left] % 2:
-                            odd_count -= 1
-                        
-                        left += 1
-                        count += 1
-            
-            nice_count += count
-
-        return nice_count
