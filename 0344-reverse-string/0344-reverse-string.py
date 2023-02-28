cb6a1866bc28f1dda5ba299608d794ea83d0723f
Olyad Temesgen Amsalu <105017653+Olyadtemesgen@users.noreply.github.com>
2023-02-28 17:49:47 +0300

    Time: 193 ms (91.46%), Space: 43.4 MB (5.49%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -4,9 +4,18 @@ class Solution:
         Do not return anything, modify s in-place instead.
         """
         left, right = 0, len(s) - 1
-        while left < right:
-            
-            s[left], s[right]= s[right], s[left]
-            right -= 1
-            left += 1
         
+#         while left < right:
+            
+#             s[left], s[right]= s[right], s[left]
+#             right -= 1
+#             left += 1
+        
+        def reverser( left, right):
+            
+            if right - left >= 1:
+                
+                s[right], s[left] = s[left], s[right]
+                reverser(left + 1, right - 1)
+        
+        reverser(left,right)
