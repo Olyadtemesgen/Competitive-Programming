e96c8d4d1fe46e4c76d5cc424f2fc097ac429d5f
Olyad Temesgen Amsalu <105017653+Olyadtemesgen@users.noreply.github.com>
2023-02-28 10:33:08 +0300

    Time: 40 ms (31.31%), Space: 13.8 MB (93.31%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.py
+++ 0342-power-of-four/0342-power-of-four.py
@@ -1,41 +1,41 @@
 class Solution:
-    def isPowerOfFour(self, n: int) -> bool:
-        
-        if n == 4 or n == 1:
-            return True
-        
-        elif n % 4 or n == 0:
-            return False
-        
-        else:
-            return self.isPowerOfFour(n / 4)
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
 #     def isPowerOfFour(self, n: int) -> bool:
         
-#         if n == 1:
+#         if n == 4 or n == 1:
 #             return True
-#         elif n == 0:
+        
+#         elif n % 4 or n == 0:
 #             return False
         
-#         while n % 4 or n == 4:
-#             n /= 4
+#         else:
+#             return self.isPowerOfFour(n / 4)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    def isPowerOfFour(self, n: int) -> bool:
         
-#         return not n % 4
+        if n == 1:
+            return True
+        elif n <= 0:
+            return False
+        
+        while not n % 4 and n != 4:
+            n /= 4
+        
+        return not n % 4
     
     
