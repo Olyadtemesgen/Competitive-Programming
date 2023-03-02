a6fb49acbe58c5e44b7d7292b9a98ad94ecd2773
Olyad Temesgen Amsalu <105017653+Olyadtemesgen@users.noreply.github.com>
2023-03-02 14:10:18 +0300

    Time: 222 ms (65.90%), Space: 15.7 MB (59.34%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.py
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.py
@@ -1,10 +1,10 @@
-from collections import deque
-
-
 class Solution:
-    def nextGreaterElements(self, nums: list[int]) -> list[int]:
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        
+        #This is a better Code I think and It
+        from collections import deque
         stack = []
-        queue = deque([])
+        
         result = [-1] * len(nums)
 
         length = len(nums)
@@ -21,3 +21,47 @@ class Solution:
         
       
         return result
+
+#This is the second code        
+        stackvalue = []
+        stackindex = []
+        result = 0
+        results = [None] * len(nums)
+        
+        for index, num in enumerate(nums):
+            
+            if not stackvalue or stackvalue[-1] >= num:
+                stackvalue.append(num)
+                stackindex.append(index)
+            
+            elif stackvalue[-1] < num:
+                
+                while stackvalue and stackvalue[-1] < num:
+                    results[stackindex.pop()] = num
+                    stackvalue.pop()
+                
+                stackindex.append(index)
+                stackvalue.append(num)
+        
+        for num in nums:
+            
+            if num > nums[-1]:
+                results[-1] = num
+                break
+        
+        for index, num in enumerate(results):
+            
+            if num == None:
+                
+                for nu in nums[:index]:
+                    
+                    if nu > nums[index]:
+                        results[index] = nu
+                        break
+        
+        for index, num in enumerate(results):
+            
+            if num == None:
+                results[index] = -1
+        
+        return results
