16ba76c1577311c8c661be3d14c0fd285b9327bd
Olyad Temesgen Amsalu <105017653+Olyadtemesgen@users.noreply.github.com>
2023-03-04 01:47:56 +0300

    Time: 228 ms (75.98%), Space: 14.6 MB (75.98%) - LeetHub

---
+++ 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.py
@@ -0,0 +1,36 @@
+class Solution:
+    def balancedString(self, s: str) -> int:
+        
+        
+        count = Counter(s)
+        length = len(s) // 4
+        left = 0
+        right = 0
+        ccs  = False
+        for cc in count:
+            if count[cc] != length:
+                ccs= True
+        
+        result = len(s)
+        if ccs:
+            for right in range(len(s)):
+                
+                count[s[right]] -= 1
+                
+                ccs = False
+                
+                for cc in count:    
+                    if count[cc] > length:
+                        ccs= True
+                
+                while not ccs and left < right and count[s[left]] < length:
+                    count[s[left]] += 1
+                    left += 1
+                
+                if not ccs:
+                    result = min(result, right - left + 1)
+                
+            return result
+        
+        else:
+            return 0
