97bd95513a3acd0fd54ff1565086cc05221b606f
Olyad Temesgen Amsalu <105017653+Olyadtemesgen@users.noreply.github.com>
2023-03-05 22:44:29 +0300

    Time: 52 ms (61.60%), Space: 14.6 MB (98.53%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -0,0 +1,9 @@
+class Solution:
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        
+        return bisect.bisect_left(nums, target)
