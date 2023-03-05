b1d0692689b5f2f34caed11aa8807b81676902e6
Olyad Temesgen Amsalu <105017653+Olyadtemesgen@users.noreply.github.com>
2023-03-06 00:00:48 +0300

    Time: 3 ms (89.28%), Space: 9.7 MB (78.72%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int searchInsert(vector<int>& arr1, int target) {
-        
+        //This is my first code that beat 100% 
         return lower_bound(arr1.begin(), arr1.end(), target)
                 - arr1.begin();
     }
