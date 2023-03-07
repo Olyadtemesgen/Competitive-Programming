c93b55be4a597f3cd27a041689a831eb008bda7d
Olyad Temesgen Amsalu <105017653+Olyadtemesgen@users.noreply.github.com>
2023-03-07 23:57:00 +0300

    Time: 135 ms (74.38%), Space: 17 MB (42.03%) - LeetHub

---
+++ 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        
+        if not root:
+            return TreeNode(val)
+        
+        def insertVal(root, val):
+            
+            if val > root.val:
+                
+                if not root.right:
+                    root.right = TreeNode(val)
+                
+                else:
+                    return insertVal(root.right, val)
+            
+            elif val < root.val:
+                
+                if not root.left:
+                    root.left = TreeNode(val)
+                
+                else:
+                    return insertVal(root.left, val)
+        
+        insertVal(root, val)
+        
+        return root
