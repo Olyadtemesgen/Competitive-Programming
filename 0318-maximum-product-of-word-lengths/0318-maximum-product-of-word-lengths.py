d0d026e3c54de4d9283a92199da69fb12e66688b
Olyad Temesgen Amsalu <105017653+Olyadtemesgen@users.noreply.github.com>
2023-03-30 22:27:41 +0300

    Time: 2098 ms (25.50%), Space: 17.7 MB (16.01%) - LeetHub

---
+++ 0318-maximum-product-of-word-lengths/0318-maximum-product-of-word-lengths.py
@@ -0,0 +1,20 @@
+class Solution:
+    
+    def maxProduct(self, words: List[str]) -> int:
+        answer = 0
+        
+        saved = []
+        
+        for x in range(len(words)):
+            saved.append(len(words[x]))
+            words[x] = set(words[x])
+            
+        for index1 in range(len(words)):
+            
+            for index2 in range(index1 + 1, len(words)):
+                
+                if len(words[index1].union(words[index2])) == len(words[index1]) + len(words[index2]):
+                    
+                    answer = max(answer, saved[index1] * saved[index2])
+            
+        return answer
