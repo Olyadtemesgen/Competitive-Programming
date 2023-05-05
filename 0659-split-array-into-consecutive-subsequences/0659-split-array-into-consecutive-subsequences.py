81c9c0687b268e980b401670e4bd6c6893896004
Olyad Temesgen Amsalu <105017653+Olyadtemesgen@users.noreply.github.com>
2023-05-05 19:06:52 +0300

    Time: 515 ms (93.56%), Space: 17.7 MB (11.88%) - LeetHub

---
+++ 0659-split-array-into-consecutive-subsequences/0659-split-array-into-consecutive-subsequences.py
@@ -0,0 +1,32 @@
+class Solution:
+    
+    def isPossible(self, nums: List[int]) -> bool:
+            
+        freq = defaultdict(int)
+
+        for num in nums:
+            freq[num] += 1
+       
+        ends = defaultdict(int)
+
+        for num in nums:
+        
+            if freq[num] == 0:
+                continue
+
+            if ends[num-1] > 0:
+                freq[num] -= 1
+                ends[num-1] -= 1
+                ends[num] += 1
+
+            elif freq[num+1] > 0 and freq[num+2] > 0:
+
+                freq[num] -= 1
+                freq[num+1] -= 1
+                freq[num+2] -= 1
+                ends[num+2] += 1
+
+            else:
+                return False
+
+        return True
