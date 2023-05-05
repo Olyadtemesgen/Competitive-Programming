a5f38c8a21adbcec0048113bab98eb825838be60
Olyad Temesgen Amsalu <105017653+Olyadtemesgen@users.noreply.github.com>
2023-05-05 14:49:34 +0300

    Time: 60 ms (48.26%), Space: 16.5 MB (7.47%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.py
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -1,40 +1,16 @@
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
         
-        dicts = defaultdict(int)
-    
-        for x in words: 
-            dicts[x] += 1
+        array = Counter(words)
+        ans = []
+        for cc in array:
+            ans.append([-array[cc], cc])
         
-        results = []
+        heapq.heapify(ans)
+        answer = []
         
-        for key, val in dicts.items():
-            results.append([val, key])
+        while k:
+            answer.append(heapq.heappop(ans)[1])
+            k -= 1
         
-        results.sort(reverse=True)
-            
-        result = []
-        
-        index = 0
-        while index < k:
-            
-            curr_val = results[index][0]
-            
-            val = []
-            while index < len(results) and curr_val == results[index][0]:
-                
-                val.append(results[index][1])
-                index += 1
-            
-            val.reverse()
-            
-            if index <= k:
-                result.extend(val)
-            
-            elif index > k:
-                while index > k:
-                    val.pop()
-                    index -= 1
-                
-                result.extend(val)
-        return result
+        return answer
