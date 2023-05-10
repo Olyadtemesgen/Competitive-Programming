9b66f09f50b448adebec8255fbf204e353834576
Olyad Temesgen Amsalu <105017653+Olyadtemesgen@users.noreply.github.com>
2023-05-10 18:49:05 +0300

    Time: 121 ms (17.53%), Space: 18 MB (12.94%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -0,0 +1,41 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        
+        graph = defaultdict(list)
+        graph2 = defaultdict(int)
+        
+        vv = set()
+        for pre in prerequisites:
+            graph[pre[1]].append(pre[0])
+            graph2[pre[0]] += 1
+            
+        
+        queue = deque([])
+        
+        for xx in range(numCourses):
+            if not graph2[xx]:
+                queue.append(xx)
+        
+        answer = []
+        
+        while queue:
+            
+            value = queue.popleft()
+            
+            answer.append(value)
+            
+            for x in graph[value]:
+                
+                graph2[x] -= 1
+                
+                if graph2[x] == 0:
+                    queue.append(x)
+        
+        if len(answer) == numCourses:
+            return answer
+        
+        return []
+            
+            
+                
+                
