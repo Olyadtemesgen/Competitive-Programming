558709afe70b67730acad08c10bae0c3a674f296
Olyad Temesgen Amsalu <105017653+Olyadtemesgen@users.noreply.github.com>
2023-05-12 02:06:37 +0300

    Time: 596 ms (27.04%), Space: 16.3 MB (19.24%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -1,24 +1,39 @@
 class Solution:
     def PredictTheWinner(self, nums: List[int]) -> bool:
-        
-        def fn(left: int, right: int, score1: int, score2: int, is_player1: bool) -> int:
+    
+        def minimax(left: int, right: int, score1: int, score2: int, is_player1: bool, alpha, beta) -> int:
+            
             if left == right:
                 return score1 + nums[left] if is_player1 else score2 + nums[left]
-
+            
             if is_player1:
-                return max(fn(left+1, right, score1+nums[left], score2, False), 
-                            fn(left, right-1, score1+nums[right], score2, False))
+                v = float('-inf')
+            
+                for i in range(2):
+                    if i == 0:
+                        v = max(v, minimax(left+1, right, score1+nums[left], score2, False, alpha, beta))
+                    else:
+                        v = max(v, minimax(left, right-1, score1+nums[right], score2, False, alpha, beta))
+                    alpha = max(alpha, v)
 
+                    if alpha >= beta:
+                        break
+
+                return v
             else:
-                return min(fn(left+1, right, score1, score2+nums[left], True), 
-                            fn(left, right-1, score1, score2+nums[right], True))
+                v = float('inf')
+                
+                for i in range(2):
+                    if i == 0:
+                        v = min(v, minimax(left+1, right, score1, score2+nums[left], True, alpha, beta))
+                    else:
+                        v = min(v, minimax(left, right-1, score1, score2+nums[right], True, alpha, beta))
+                    beta = min(beta, v)
+                    if alpha >= beta:
+                        break
+                
+                return v
 
-        
         total_score = sum(nums)
-
-                # memo = {}
-
-        player1_score = fn(0, len(nums)-1, 0, 0, True)
-        print(player1_score)
-        return  player1_score >= (sum(nums) - player1_score)
-    
+        player1_score = minimax(0, len(nums)-1, 0, 0, True, float('-inf'), float('inf'))
+        return player1_score >= (total_score - player1_score)
