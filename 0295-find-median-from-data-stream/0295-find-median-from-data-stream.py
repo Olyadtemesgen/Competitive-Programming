9e3d04353fb8dc962b4cec9bc7c27147b6781971
Olyad Temesgen Amsalu <105017653+Olyadtemesgen@users.noreply.github.com>
2023-05-12 17:59:38 +0300

    Time: 471 ms (98.12%), Space: 38.4 MB (11.12%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -1,29 +1,60 @@
 from sortedcontainers import SortedList
+
 class MedianFinder:
     
     def __init__(self):
         
-        self.heap = SortedList()
+        self.heap = []
+        heapq.heapify(self.heap)
+        
+        self.heap2 = []
+        heapq.heapify(self.heap2)
 
     def addNum(self, num: int) -> None:
-        # if not self.heap:
-        self.heap.add(num)
-        # print(self.heap)
+
+        
+        if not self.heap and not self.heap2:
+            self.heap2.append(num)
+        
+        elif self.heap2 and not self.heap:
+            
+            if self.heap2[0] >= num:
+                heapq.heappush(self.heap, -num)
+            
+            else:
+                heapq.heappush(self.heap, -heapq.heappop(self.heap2))
+                heapq.heappush(self.heap2, num)
+        
+        else:
+            
+            if self.heap2[0] < num:
+                heapq.heappush(self.heap2, num)
+                
+                if len(self.heap2) - 2 == len(self.heap):
+                    heapq.heappush(self.heap, -heapq.heappop(self.heap2))
+            
+            else:
+                
+                heapq.heappush(self.heap, -num)
+                
+                if len(self.heap) - 2 == len(self.heap2):
+                    heapq.heappush(self.heap2, -heapq.heappop(self.heap))
+        # print(self.heap, self.heap2)
         
     def findMedian(self) -> float:
         
-        if len(self.heap) % 2:
-            return self.heap[len(self.heap) // 2]
+        if not self.heap:
+            return self.heap2[0]
+        
+        if len(self.heap) == len(self.heap2):
+            return (-self.heap[0] + self.heap2[0]) / 2
+        
+        elif len(self.heap) > len(self.heap2):
+            return -self.heap[0]
         
         else:
-            length = len(self.heap) // 2
-            
-            val1 = self.heap[length]
-            val2 = self.heap[length - 1]
-            
-            return (val1 + val2) / 2
-            
-
+            return self.heap2[0]
+        
 # Your MedianFinder object will be instantiated and called as such:
 # obj = MedianFinder()
 # obj.addNum(num)
