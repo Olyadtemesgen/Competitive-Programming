2ba1d4706c88657dff072410118f2af7943688e7
Olyad Temesgen Amsalu <105017653+Olyadtemesgen@users.noreply.github.com>
2023-05-23 01:14:25 +0300

    Time: 67 ms (15.01%), Space: 16.3 MB (45.84%) - LeetHub

--- 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.py
+++ 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.py
@@ -21,15 +21,11 @@ class DisjointSet:
         ch2 = self.find(ch2)
         
         
-        if self.rank[(ord(ch1)) - 97] > self.rank[(ord(ch2)) - 97]:
+        if ch1 > ch2:
             self.parent[(ord(ch1)) - 97] = ch2
-
-        elif self.rank[(ord(ch1)) - 97] < self.rank[(ord(ch2)) - 97]:
-            self.parent[(ord(ch2)) - 97] = ch1
 
         else:
-            self.parent[(ord(ch1)) - 97] = ch2
-            self.rank[(ord(ch2)) - 97] += 1
+            self.parent[(ord(ch2)) - 97] = ch1
     
     def is_connected(self, ch1, ch2):
         return self.find(ch1) == self.find(ch2)
@@ -47,15 +43,10 @@ class Solution:
             if not ds.is_connected(s1[index], s2[index]):
                 ds.union(s1[index], s2[index])
         
-        for ind1 in range(97, 123):
-            for ind2 in range(97, ind1):
-                if ds.is_connected(chr(ind1), chr(ind2)):
-                    swap[chr(ind1)] = chr(ind2)
-                    break
         
         answer = []
         
         for ss in baseStr:
-            answer.append(swap[ss])
+            answer.append(ds.find(ss))
         
         return "".join(answer)
