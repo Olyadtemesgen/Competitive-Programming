413a7dcd66db3562f42c4e4d344f53ca41a8a779
Olyad Temesgen Amsalu <105017653+Olyadtemesgen@users.noreply.github.com>
2023-05-25 18:30:19 +0300

    Time: 519 ms (39.75%), Space: 34.8 MB (6.83%) - LeetHub

--- 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.py
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.py
@@ -6,7 +6,9 @@ class Solution:
         arr[0] = 1
         
         for index in range(1, len(arr)):
+            
             if arr[index] - arr[index - 1] > 1:
+                
                 arr[index] = arr[index - 1] + 1
         
         return len(Counter(arr))
