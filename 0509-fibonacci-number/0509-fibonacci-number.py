f706dd9594ed7bc2b2636ea06147c0a53e36769e
Olyad Temesgen Amsalu <105017653+Olyadtemesgen@users.noreply.github.com>
2023-06-01 18:53:26 +0300

    Time: 951 ms (5.07%), Space: 16.4 MB (7.44%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,11 +1,14 @@
 class Solution:
+    
+    def __init__(self):
+        self.memoization = {}
+    
     def fib(self, n: int) -> int:
         
-        memory = {0: 0, 1: 1}
+        if n in self.memoization:
+            return self.memoization
         
-        if n in memory:
-            return memory[n]
+        if n <= 1:
+            return n
         
-        else:
-            memory[n] = memory.get(n - 1, 0) + memory.get(n - 2 , 0)
-            return self.fib(n - 1) + self.fib(n - 2)
+        return self.fib(n - 1) + self.fib(n - 2)
