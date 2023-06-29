2c06a362ab7e01b1e52563873f25b14fb26b9d40
Olyad Temesgen Amsalu <105017653+Olyadtemesgen@users.noreply.github.com>
2023-06-29 19:54:17 +0300

    Time: 625 ms (5.03%), Space: 84 MB (5.91%) - LeetHub

---
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
@@ -0,0 +1,26 @@
+class Solution:
+    def maxProfit(self, k: int, prices: List[int]) -> int:
+        
+        demo = {}
+        
+        def brute_force(index, turn, num):
+            
+            if index >= len(prices) or num > k - 1:
+                return 0
+            
+            if (index, turn, num - 1) in demo:
+                return demo[(index, turn, num - 1)]
+            
+            elif (index, turn, num) in demo:
+                return demo[(index, turn, num)]
+            
+            if turn:
+                 demo[(index, turn, num)] = max(brute_force(index + 1, False, num) - prices[index], brute_force(index + 1, turn, num))
+            
+            else:
+                demo[(index, turn, num)] = max(brute_force(index + 1, True, num + 1) + prices[index], brute_force(index + 1, turn, num))
+            
+            return demo[(index, turn, num)]
+        
+        return brute_force(0, True, 0)
+            
