7cb44dc88a447db634f1958e20b5cef1dd7e0e58
Olyad Temesgen Amsalu <105017653+Olyadtemesgen@users.noreply.github.com>
2023-07-10 10:26:16 +0300

    Time: 109 ms (66.20%), Space: 16.3 MB (91.20%) - LeetHub

--- 0799-champagne-tower/0799-champagne-tower.py
+++ 0799-champagne-tower/0799-champagne-tower.py
@@ -1,22 +1,23 @@
 class Solution:
     def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
         
+        # first lets make a dp list of lists that we will store the amount that the glass will hold
+        
         dp = [[0 for _ in range(x)] for x in range(1, query_row + 2)]
         
         dp[0][0] = poured
         
-        for i in range(query_row):
-            for j in range(len(dp[i])):
+        for index in range(query_row):
+            
+            for before in range(len(dp[index])):
                 
-                temp = (dp[i][j] - 1) / 2.0
+                overflowed = ((dp[index][before]) - 1) / 2
                 
-                if temp>0:
-                    dp[i+1][j] += temp
-                    dp[i+1][j+1] += temp
+                if overflowed > 0:
+                    dp[index + 1][before] += overflowed
+                    dp[index + 1][before + 1] += overflowed
         
-        return dp[query_row][query_glass] if dp[query_row][query_glass] <= 1 else 1
-        
-        
+        return min(1, dp[query_row][query_glass])
         
         
         
