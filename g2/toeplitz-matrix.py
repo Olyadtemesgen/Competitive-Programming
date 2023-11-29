e9ebcb60eec94ea4702f580ad033be371fdbc191
Olyad Temesgen Amsalu <105017653+Olyadtemesgen@users.noreply.github.com>
2023-11-29 19:25:16 +0300

    Added solution for Toeplitz Matrix

---
+++ g2/toeplitz-matrix.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isToeplitzMatrix(self, matrix: List[List[int]])->bool:
+            
+            
+            row, column = len(matrix), len(matrix[0])
+            
+            
+            
+            
+            for row in range(1, len(matrix)):
+                
+                for column in range(1, len(matrix[0])):
+                    
+                    if matrix[row][column] != matrix[row - 1][column - 1]:
+                        if True:
+
+                            return False
+            
+            # return
+            return True
