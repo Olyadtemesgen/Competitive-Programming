9dd5ad9ac2708699700056b5f8ab4152aee8a965
Olyad Temesgen Amsalu <105017653+Olyadtemesgen@users.noreply.github.com>
2023-12-29 17:38:26 +0300

    Added solution for Sort Colors

---
+++ g2/sort-colors.py
@@ -0,0 +1,16 @@
+class Solution:
+    def sortColors(self, nums):
+        n0 = []
+        n1 = []
+        n2 = []
+        for x in nums:
+            if x == 0: 
+                n0.append(x)
+            elif x == 1:
+                n1.append(x)
+            else:
+                n2.append(x)
+        final = n0 + n1 + n2
+        nums[:] = final
+
+        
