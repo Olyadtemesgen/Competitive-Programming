bf5a5ab706fe38983a042f202d2450f0c1ab618e
Olyad Temesgen Amsalu <105017653+Olyadtemesgen@users.noreply.github.com>
2023-12-30 21:09:09 +0300

    Added solution for Reverse String

--- g2/reverse-string.py
+++ g2/reverse-string.py
@@ -5,8 +5,12 @@ class Solution:
         """
 
         half = len(s) // 2
+        
         for idx in range(len(s)):
+            
             if not half:
                 break
+            
             half -= 1
+            
             s[idx], s[len(s) - 1 - idx] = s[len(s) - 1 - idx] , s[idx]        
