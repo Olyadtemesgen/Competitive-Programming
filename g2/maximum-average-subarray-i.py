9e467ed810dbfc9426c9d3773370ecfe2eda2c73
Olyad Temesgen Amsalu <105017653+Olyadtemesgen@users.noreply.github.com>
2024-01-10 18:12:55 +0300

    Added solution for Maximum Average Subarray I

---
+++ g2/maximum-average-subarray-i.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        
+        right = k
+        left = 0
+        
+        summ = sum(nums[:right])
+        
+        maximum = summ
+        
+        while right < len(nums):
+            summ = summ - nums[left] + nums[right]
+            
+            left += 1
+            right += 1
+            
+            maximum = max(summ, maximum)
+            
+        return maximum / k
