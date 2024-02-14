477b1d8562fc0c3d79066b0cd70c2053255dce65
Olyad Temesgen Amsalu <105017653+Olyadtemesgen@users.noreply.github.com>
2024-02-14 09:47:22 +0300

    Add solution for K Items With the Maximum Sum

---
+++ g2/leetcode/k-items-with-the-maximum-sum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def kItemsWithMaximumSum(self, numOnes: int, numZeros: int, numNegOnes: int, k: int) -> int:
+        
+        if numOnes >= k:
+            return k
+        #cccc
+        elif numOnes + numZeros >= k:
+            return numOnes
+        
+        return (numOnes) - abs(numOnes + numZeros - k)
