d484853326882f944b8bef960e91f33064c8eafa
Olyad Temesgen Amsalu <105017653+Olyadtemesgen@users.noreply.github.com>
2024-05-02 10:30:14 +0300

    Update 0001-two-sum.py

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,6 +1,6 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for x in range(len(nums) - 1):
-            
+            # it could be brtf tho.
             if target - nums[x]- 1 + 1 in nums[x + 1:]:
                 return [x, nums[x + 1:].index(target - nums[x]) + x + 1]
